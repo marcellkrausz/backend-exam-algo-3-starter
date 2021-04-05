f4d8255959ff9811e07e4972cf54f99cfe745767
marcellkrausz <krauszmaarcell@gmail.com>
2021-04-05 14:05:07 +0200

    add minPositive

--- src/main/java/com/codecool/Challenge.java
+++ src/main/java/com/codecool/Challenge.java
@@ -3,6 +3,12 @@ package com.codecool;
 public class Challenge {
 
     public static int minPositive(int a, int b) {
-
+        if (a > 0 && a <= b || a > 0 && b <= 0) {
+            return a;
+        } else if (b > 0) {
+            return b;
+        } else {
+            return -1;
+        }
     }
 }
